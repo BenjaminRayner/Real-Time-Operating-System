4b4b523612eec9dd79eedb134e6044ee9cc2baa6
Benjamin Rayner <90818781+BenjaminRayner@users.noreply.github.com>
2024-09-29 11:47:12 -0300

    Add files via upload

---
+++ AE-Lib/src/ae/ae_tasks200_G99.c
@@ -0,0 +1,170 @@
+/*
+ ****************************************************************************
+ *
+ *                  UNIVERSITY OF WATERLOO ECE 350 RTOS LAB
+ *
+ *                     Copyright 2020-2021 Yiqing Huang
+ *                          All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice and the following disclaimer.
+ *
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS AND CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ ****************************************************************************
+ */
+
+/**************************************************************************//**
+ * @file        ae_tasks200.c
+ * @brief       P2 public testing suite 200
+ *
+ * @version     V1.2021.06
+ * @authors     Yiqing Huang
+ * @date        2021 Jun
+ * *
+ *****************************************************************************/
+
+#include "ae_tasks.h"
+#include "uart_polling.h"
+#include "printf.h"
+#include "ae.h"
+#include "ae_util.h"
+#include "ae_tasks_util.h"
+#include "mailbox.h"
+
+/*
+ *===========================================================================
+ *                             MACROS
+ *===========================================================================
+ */
+    
+#define NUM_TESTS       1       // number of tests
+#define NUM_INIT_TASKS  1       // number of tasks during initialization
+/*
+ *===========================================================================
+ *                             GLOBAL VARIABLES 
+ *===========================================================================
+ */
+
+TASK_INIT   g_init_tasks[NUM_INIT_TASKS];
+const char   PREFIX[]      = "G37-TS1";
+const char   PREFIX_LOG[]  = "G37-TS1-LOG";
+const char   PREFIX_LOG2[] = "G37-TS1-LOG2";
+
+AE_XTEST     g_ae_xtest;                // test data, re-use for each test
+AE_CASE      g_ae_cases[NUM_TESTS];
+AE_CASE_TSK  g_tsk_cases[NUM_TESTS];
+
+void set_ae_init_tasks (TASK_INIT **pp_tasks, int *p_num)
+{
+    *p_num = NUM_INIT_TASKS;
+    *pp_tasks = g_init_tasks;
+    set_ae_tasks(*pp_tasks, *p_num);
+}
+
+// initial task configuration
+void set_ae_tasks(TASK_INIT *tasks, int num)
+{
+    for (int i = 0; i < num; i++ ) {                                                 
+        tasks[i].u_stack_size = PROC_STACK_SIZE;    
+        tasks[i].prio = HIGH + i;
+        tasks[i].priv = 1;
+    }
+    tasks[0].priv  = 1;
+    tasks[0].ptask = &priv_task1;
+    
+    init_ae_tsk_test();
+}
+
+void init_ae_tsk_test(void)
+{
+    g_ae_xtest.test_id = 0;
+    g_ae_xtest.index = 0;
+    g_ae_xtest.num_tests = NUM_TESTS;
+    g_ae_xtest.num_tests_run = 0;
+    
+    for ( int i = 0; i< NUM_TESTS; i++ ) {
+        g_tsk_cases[i].p_ae_case = &g_ae_cases[i];
+        g_tsk_cases[i].p_ae_case->results  = 0x0;
+        g_tsk_cases[i].p_ae_case->test_id  = i;
+        g_tsk_cases[i].p_ae_case->num_bits = 0;
+        g_tsk_cases[i].pos = 0;  // first avaiable slot to write exec seq tid
+        // *_expt fields are case specific, deligate to specific test case to initialize
+    }
+    printf("%s: START\r\n", PREFIX);
+}
+
+void update_ae_xtest(int test_id)
+{
+    g_ae_xtest.test_id = test_id;
+    g_ae_xtest.index = 0;
+    g_ae_xtest.num_tests_run++;
+}
+
+void gen_req0(int test_id)
+{
+    // bits[0:1] for two tsk_create, [2:5] for 4 tsk_yield return value checks
+    g_tsk_cases[test_id].p_ae_case->num_bits = 6;  
+    g_tsk_cases[test_id].p_ae_case->results = 0;
+    g_tsk_cases[test_id].p_ae_case->test_id = test_id;
+    g_tsk_cases[test_id].len = 16; // assign a value no greater than MAX_LEN_SEQ
+    g_tsk_cases[test_id].pos_expt = 6;
+       
+    g_ae_xtest.test_id = test_id;
+    g_ae_xtest.index = 0;
+    g_ae_xtest.num_tests_run++;
+}
+
+void gen_req1(int test_id)
+{
+    //bits[0:3] pos check, bits[4:9] for exec order check
+    g_tsk_cases[test_id].p_ae_case->num_bits = 10;  
+    g_tsk_cases[test_id].p_ae_case->results = 0;
+    g_tsk_cases[test_id].p_ae_case->test_id = test_id;
+    g_tsk_cases[test_id].len = 0;       // N/A for this test
+    g_tsk_cases[test_id].pos_expt = 0;  // N/A for this test
+    
+    update_ae_xtest(test_id);
+}
+
+void priv_task1(void)
+{	
+		mbx_create(0x100);
+	
+		size_t msg_hdr_size = sizeof(struct rtx_msg_hdr);
+		U8 *buf = mem_alloc(msg_hdr_size + 1); /* buffer is allocated by the caller somewhere else*/
+		struct rtx_msg_hdr *ptr = (void *)buf;
+		ptr->length = msg_hdr_size + 1;
+		ptr->type = KCD_REG;
+		ptr->sender_tid = tsk_gettid();
+		buf += msg_hdr_size;
+		*buf = 'W';
+		send_msg(TID_KCD, (void *)ptr);
+		*buf = 'C';
+		send_msg(TID_KCD, (void *)ptr);
+	
+		while (1) {
+			tsk_yield();
+		}
+	
+    tsk_exit();
+}
+
+
+/*
+ *===========================================================================
+ *                             END OF FILE
+ *===========================================================================
+ */
---
+++ AE-Lib/src/ae/ae_tasks300_G99.c
@@ -0,0 +1,202 @@
+/**************************************************************************//**
+ * @file        ae_tasks300.c
+ * @brief       P3 Test Suite 300  - Basic Non-blocking Message Passing
+ *
+ * @version     V1.2021.07
+ * @authors     Yiqing Huang
+ * @date        2021 Jul
+ *
+ * @note        Each task is in an infinite loop. These Tasks never terminate.
+ *
+ *****************************************************************************/
+
+#include "ae_tasks.h"
+#include "uart_polling.h"
+#include "printf.h"
+#include "ae_util.h"
+#include "ae_tasks_util.h"
+
+/*
+ *===========================================================================
+ *                             MACROS
+ *===========================================================================
+ */
+
+#define     NUM_TESTS       2       // number of tests
+#define     NUM_INIT_TASKS  1       // number of tasks during initialization
+#define     BUF_LEN         128     // receiver buffer length
+#define     MY_MSG_TYPE     100     // some customized message type
+
+/*
+ *===========================================================================
+ *                             GLOBAL VARIABLES
+ *===========================================================================
+ */
+const char   PREFIX[]      = "G99-TS300";
+const char   PREFIX_LOG[]  = "G99-TS300-LOG";
+const char   PREFIX_LOG2[] = "G99-TS300-LOG2";
+TASK_INIT    g_init_tasks[NUM_INIT_TASKS];
+
+AE_XTEST     g_ae_xtest;                // test data, re-use for each test
+AE_CASE      g_ae_cases[1];
+AE_CASE_TSK  g_tsk_cases[NUM_TESTS];
+
+/* The following arrays can also be dynamic allocated to reduce ZI-data size
+ *  They do not have to be global buffers (provided the memory allocator has no bugs)
+ */
+
+U8 g_buf1[BUF_LEN];
+U8 g_buf2[BUF_LEN];
+task_t g_tasks[MAX_TASKS];
+task_t g_tids[MAX_TASKS];
+
+void set_ae_init_tasks (TASK_INIT **pp_tasks, int *p_num)
+{
+    *p_num = NUM_INIT_TASKS;
+    *pp_tasks = g_init_tasks;
+    set_ae_tasks(*pp_tasks, *p_num);
+}
+
+void set_ae_tasks(TASK_INIT *tasks, int num)
+{
+    for (int i = 0; i < num; i++ ) {
+        tasks[i].u_stack_size = PROC_STACK_SIZE;
+        tasks[i].prio = HIGH;
+        tasks[i].priv = 0;
+    }
+
+    tasks[0].ptask = &task0;
+
+    init_ae_tsk_test();
+}
+
+void init_ae_tsk_test(void)
+{
+    g_ae_xtest.test_id = 0;
+    g_ae_xtest.index = 0;
+    g_ae_xtest.num_tests = NUM_TESTS;
+    g_ae_xtest.num_tests_run = 0;
+
+    for ( int i = 1; i< NUM_TESTS; i++ ) {
+        g_tsk_cases[i].p_ae_case = &g_ae_cases[i];
+        g_tsk_cases[i].p_ae_case->results  = 0x0;
+        g_tsk_cases[i].p_ae_case->test_id  = i;
+        g_tsk_cases[i].p_ae_case->num_bits = 0;
+        g_tsk_cases[i].pos = 0;  // first avaiable slot to write exec seq tid
+        // *_expt fields are case specific, deligate to specific test case to initialize
+    }
+    printf("%s: START\r\n", PREFIX);
+}
+
+void update_ae_xtest(int test_id)
+{
+    g_ae_xtest.test_id = test_id;
+    g_ae_xtest.index = 0;
+    g_ae_xtest.num_tests_run++;
+}
+
+void gen_req0(int test_id)
+{
+    g_tsk_cases[test_id].p_ae_case->num_bits = 12;
+    g_tsk_cases[test_id].p_ae_case->results = 0;
+    g_tsk_cases[test_id].p_ae_case->test_id = test_id;
+    g_tsk_cases[test_id].len = 16; // assign a value no greater than MAX_LEN_SEQ
+    g_tsk_cases[test_id].pos_expt = 9;
+
+    update_ae_xtest(test_id);
+}
+
+void gen_req1(int test_id)
+{
+    //bits[0:3] pos check, bits[4:12] for exec order check
+    g_tsk_cases[test_id].p_ae_case->num_bits = 3; //Change to increase number of p_index
+    g_tsk_cases[test_id].p_ae_case->results = 0;
+    g_tsk_cases[test_id].p_ae_case->test_id = test_id;
+    g_tsk_cases[test_id].len = 0;       // N/A for this test
+    g_tsk_cases[test_id].pos_expt = 0;  // N/A for this test
+
+    update_ae_xtest(test_id);
+}
+
+/**
+ * @brief   task yield exec order test
+ * @param   test_id, the test function ID
+ * @param   ID of the test function that logs the testing data
+ * @note    usually test data is logged by the same test function,
+ *          but some time, we may have multiple tests to check the same test data
+ *          logged by a particular test function
+ */
+void test1_start(int test_id, int test_id_data)
+{
+    gen_req0(0);
+    gen_req1(1);
+
+    U8      pos         = g_tsk_cases[test_id_data].pos;
+    U8      pos_expt    = g_tsk_cases[test_id_data].pos_expt;
+    task_t  *p_seq      = g_tsk_cases[test_id_data].seq;
+    task_t  *p_seq_expt = g_tsk_cases[test_id_data].seq_expt;
+
+    U8      *p_index    = &(g_ae_xtest.index);
+    int     sub_result  = 0;
+
+    (*p_index)++;
+    strcpy(g_ae_xtest.msg, "creating a giant mailbox of size 6969420 Bytes");
+    mbx_t mbx_id1 = mbx_create(6969420);
+    sub_result = (mbx_id1 < 0) ? 1 : 0;
+    process_sub_result(test_id, *p_index, sub_result);
+
+    (*p_index)++;
+    sprintf(g_ae_xtest.msg, "creating a small mailbox of size %u Bytes", 1);
+    mbx_t mbx_id2 = mbx_create(1);
+    sub_result = (mbx_id2 < 0) ? 1 : 0;
+    process_sub_result(test_id, *p_index, sub_result);
+
+    (*p_index)++;
+    sprintf(g_ae_xtest.msg, "creating repeat mailbox of size %u Bytes", 128);
+    mbx_t mbx_ida = mbx_create(128);
+    mbx_t mbx_idb = mbx_create(128);
+    sub_result = (mbx_idb < 0) ? 1 : 0;
+    process_sub_result(test_id, *p_index, sub_result);
+
+    test_exit();
+}
+
+int update_exec_seq(int test_id, task_t tid)
+{
+
+    U8 len = g_tsk_cases[test_id].len;
+    U8 *p_pos = &g_tsk_cases[test_id].pos;
+    task_t *p_seq = g_tsk_cases[test_id].seq;
+    p_seq[*p_pos] = tid;
+    (*p_pos)++;
+    (*p_pos) = (*p_pos)%len;  // preventing out of array bound
+    return RTX_OK;
+}
+
+/**************************************************************************//**
+ * @brief   The first task to run in the system
+ *****************************************************************************/
+
+void task0(void)
+{
+    int ret_val = 10;
+    //mbx_t mbx_id = -1;
+    task_t tid = tsk_gettid();
+    g_tids[0] = tid;
+    int     test_id    = 0;
+    U8      *p_index   = &(g_ae_xtest.index);
+    int     sub_result = 0;
+
+    printf("%s: TID = %u, task0 entering\r\n", PREFIX_LOG2, tid);
+    update_exec_seq(test_id, tid);
+
+    tsk_yield(); // let the other two tasks to run
+    update_exec_seq(test_id, tid);
+    test1_start(test_id + 1, test_id);
+}
+
+/*
+ *===========================================================================
+ *                             END OF FILE
+ *===========================================================================
+ */
